d8021024a24f0b825663dc8db304d7bf0075ef73
Navya-PampariRaghu <95553997+Navya-PampariRaghu@users.noreply.github.com>
2021-12-05 11:54:44 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ def edit():
     #Data to pass to our templates/index.html file
     data = 6
 
-    #This line means render index.html with params data as name game_data
-    #A great behavior that can be very useful.
+    
     return render_template('index.html', game_data = data)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug = True)
